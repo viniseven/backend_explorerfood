304ab7c9d9465b1e79a3c715aa11d0b5766e5b4f
Marcus Vinicius <marcusv892@gmail.com>
2023-05-30 19:51:10 -0300

    feat: Create featura update data user and encrypt password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -25,6 +25,54 @@ class UsersController {
  
     return response.status(201).json('Usuário cadastrado com sucesso')
   }
+
+  async update(request, response) {
+    const { id } = request.params
+    const { name, email, newPassword, oldPassword } = request.body
+
+    const database = await sqlConnection();
+
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user){
+      throw new AppError('Usuário não encontrado')
+    }
+
+    const userWithUpdateEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdateEmail && userWithUpdateEmail.id !== id){
+      throw new AppError('Este email já está em uso')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(newPassword && !oldPassword){
+      throw new AppError('Por favor, insira a senha antiga')
+    }
+
+    if(newPassword && oldPassword){
+      const checkOldPassword = await bcrypt.compare(oldPassword, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError('A senha antiga não confere')
+      }
+
+      const checkEqualsPassword = await bcrypt.compare(newPassword, user.password)
+
+      if(checkEqualsPassword){
+        throw new AppError('A senha antiga não pode ser igual a atual')
+      }
+    }
+
+    const password = await bcrypt.hash(newPassword, 8)
+
+    await database.run(`UPDATE users SET name = ?, email = ?, password = ?, updated_at = DATETIME("now")
+    WHERE id = ?`, [user.name, user.email, password, id])
+
+    return response.json('Dados alterados com sucesso')
+
+  }
 }
 
 module.exports = UsersController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router();
 const userController = new UsersController()
 
 usersRoutes.post('/', userController.create)
+usersRoutes.put('/:id', userController.update)
 
 module.exports = usersRoutes;
