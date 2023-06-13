c84a8b0193b74eec1d01c9028d565d897788f5f8
Marcus Vinicius <marcusv892@gmail.com>
2023-06-13 19:00:27 -0300

    feat: delete dishe

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,4 +1,5 @@
 const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
 
 class DishesController {
   async create(request, response){
@@ -17,6 +18,51 @@ class DishesController {
     await knex('ingredients').insert(ingredientsInsert)
 
     return response.json('Prato cadastrado com sucesso')
+  }
+
+  async update(request, response){
+    const { id } = request.params
+    const { name, category, ingredients, price, description } = request.body
+
+    const dishe = await knex('dishes').where({id}).first()
+
+    if(!dishe){
+      throw new AppError('Este prato não existe')
+    }
+
+    dishe.name = name ?? dishe.name
+    dishe.category = category ?? dishe.category
+    dishe.ingredients = ingredients ?? dishe.ingredients
+    dishe.price = price ?? dishe.price
+    dishe.description = description ?? dishe.description
+
+    await knex('dishes')
+    .where({ id })
+    .update({name: dishe.name, category: dishe.category, price:dishe.price, 
+      description: dishe.description})
+
+   const ingredientsUpdate = ingredients.map(ingredient => {
+    return {
+      name: ingredient
+    }
+  })
+
+  await knex('ingredients').where({ dishe_id: id }).update({ name: ingredientsUpdate })
+    
+  return response.json('Prato atualizado com sucesso')
+
+  }
+
+  async delete(request, response){
+      const { id } = request.params
+
+      await knex('dishes').where({ id }).del()
+
+      return response.json('Prato excluído com sucesso')
+  }
+
+  async show(request, response){
+    const { id } = request.params
   }
 }
 
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -7,6 +7,8 @@ const dishesRoutes = Router();
 const dishesController = new DishesController()
 
 dishesRoutes.post('/:user_id', dishesController.create)
+dishesRoutes.put('/:id', dishesController.update)
+dishesRoutes.delete('/:id', dishesController.delete)
 
 module.exports = dishesRoutes
 
